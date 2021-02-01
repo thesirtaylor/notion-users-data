4f99ac479c421bd8e0479f48f94b05ceedd5c0cd
thesirtaylor <ajayiolatunji17@gmail.com>
2021-02-01 09:35:43 +0100

    first slave commit

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,8 +1,15 @@
 import React from "react";
 const Display = (props) => {
-  return props.list.map((person) => {
+  if (!props) {
     return (
-      <div className=" ui cards">
+      <div>
+        <h1>Loading</h1>
+      </div>
+    );
+  }
+  return props.list.map((person, index) => {
+    return (
+      <div className=" ui cards" key={person.CreditCardNumber}>
         <div className="card">
           <div className="ui">
             <div className="header">
@@ -11,9 +18,7 @@ const Display = (props) => {
             <div className="header">{person.Email}</div>
             <div className="header">{person.PhoneNumber}</div>
             <div className="meta">{person.Gender}</div>
-            <div className="meta">
-              {person.URL}
-            </div>
+            <div className="meta">{person.URL}</div>
             <div className="meta">
               {person.Longitude + "," + person.Latitude}
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,10 @@ const Header = (props) => {
       {/* <div className="header"> */}
         <h2 className="item">Hello World</h2>
         <div className="right menu">
-          <div class="item">
-            <div class="ui icon input">
+          <div className="item">
+            <div className="ui icon input">
               <input type="text" placeholder="Search..." />
-              <i class="search link icon"></i>
+              <i className="search link icon"></i>
             </div>
           </div>
         </div>
